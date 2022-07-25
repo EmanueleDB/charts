dac64acba4019b2667f0bc3db1e3326aecfa296a
emanueledb <emanueledibene@gmail.com>
2022-07-25 18:36:53 +0200

    gitignore comment inside

---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Home</div>
+</template>
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Home",
+})
+</script>
