57b928d639b84f95dec60efb6a98244720d7dfe7
emanueledb <emanueledibene@gmail.com>
2022-07-25 18:52:52 +0200

    ts implementation completed

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,7 @@
     <router-view />
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({})
+</script>
--- src/components/testComponent/TestComponent.vue
+++ src/components/testComponent/TestComponent.vue
@@ -4,7 +4,6 @@
 <script lang="ts">
 import Vue from "vue"
 export default Vue.extend({
-  // eslint-disable-next-line vue/multi-word-component-names
   name: "TestComponent",
 })
 </script>
