6ef84b95fb051438c44aa2fddf5aa2ea58200259
emanueledb <emanueledibene@gmail.com>
2022-10-02 14:28:26 +0200

    Icons added

--- src/components/testComponent/TestComponent.vue
+++ src/components/testComponent/TestComponent.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="row">
     <div class="col-6">
-      <b-alert variant="success" show>Success Alert</b-alert>
-      <p>PR/communicatiestrategie en uitvoering: Ik help met het verbinden van de communicatiestrategie aan de organisatiedoelen. Perscommunicatie loont als je het goed doet. Maar een verhaal gepubliceerd krijgen is niet eenvoudig. Vertrouw daarom op de expertise van TKN.
-        Imagoverbetering en stakeholder- en reputatiemanagement: Hulp nodig bij het opstellen van communicatietrajecten met het oog op imagoverbetering en stakeholder- en reputatiemanagement? TKN help in kaart te brengen welke issues voor jouw stakeholders belangrijk zijn én waar je impact op hebt.
-        Communicatie campagnes: Ik kan helpen met het bedenken, coördineren en uitvoeren van innovatieve communicatie campagnes & content die binnenkomt bij de ontvanger.
-        Copywriting: Van nieuwsbericht tot brand story of online content, ik vind de juiste tone of voice richting de gewenste doelgroep.
-      </p>
+      <div class="container">
+        <b-alert variant="success" show>Success Alert</b-alert>
+        <p>test</p>
+        <FontAwesomeIcon icon="fa-solid fa-language" />
+      </div>
     </div>
   </div>
 </template>
