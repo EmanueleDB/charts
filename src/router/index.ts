be3ff9bd029abfc5882cccd360706462877ddf16
emanueledb <emanueledibene@gmail.com>
2022-11-29 16:24:57 +0100

    scroll to top added

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,9 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
 })
 
 export default router
