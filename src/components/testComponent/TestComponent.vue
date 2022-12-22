6688c66864ac1d839733cb50b830e20b6093fb4b
emanueledb <emanueledibene@gmail.com>
2022-12-22 17:24:16 +0100

    bootstrap version updated

--- src/components/testComponent/TestComponent.vue
+++ src/components/testComponent/TestComponent.vue
@@ -2,8 +2,11 @@
   <div class="row">
     <div class="col-6">
       <div class="container">
-        <div class="alert alert-success show">Success Alert</div>
-        <p>test</p>
+        <div class="text-center">
+          <div class="text-center my-3">
+            <b-progress :value="35" :max="100" show-progress animated></b-progress>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
+import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(BootstrapVue3).mount('#app')
