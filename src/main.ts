e69c5b246a0cee25c48387ee1d541d98e2615f4a
emanueledb <emanueledibene@gmail.com>
2023-05-19 17:26:10 +0200

    npm --> pnpm  / bootstrap updated

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
-import BootstrapVue3 from 'bootstrap-vue-3'
+import { BootstrapVueNext } from 'bootstrap-vue-next'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const pinia = createPinia()
 
-createApp(App).use(router).use(pinia).use(BootstrapVue3).mount('#app')
+createApp(App).use(router).use(pinia).use(BootstrapVueNext).mount('#app')
