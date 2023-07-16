7a9d7719b5847d5db04bcc59f0f9ee35c248c119
emanueledb <emanueledibene@gmail.com>
2023-07-16 13:28:32 +0200

    fix for the data

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export async function fetchAssets() {
-  const response = await axios.get('/data/assets.json')
+  const response = await axios.get('/assets.json')
   return response.data
 }
--- src/data/measurements.ts
+++ src/data/measurements.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export async function fetchMeasurements() {
-  const response = await axios.get('/data/measurements.json')
+  const response = await axios.get('/measurements.json')
   return response.data
 }
